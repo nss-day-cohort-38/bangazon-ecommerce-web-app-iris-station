69a63084561e3ea1f6ec9373e6a8c2bad90754b1
Kurt Krafft <kkrafft6609@gmail.com>
2020-05-26 11:54:58 -0500

    finished adding home cards to list

---
+++ src/pages/home/HomePage.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react';
+import productManager from "../../modules/productManager"
+import HomeListCard from "../../components/cards/HomeListCard"
+import "./HomePage.css"
+
+const HomePage = props => {
+    const [prods, setProds] = useState([])
+
+
+    useEffect(()=> {
+        productManager.getHomeList().then(arr=> {
+            setProds(arr)
+        })
+    })
+    return (
+        <>
+        <div className="home-header"><h1>Welcome Back!</h1><h4>Here's what's new...</h4></div>
+        <div className = "list-container">
+            {prods.map(product=> (
+                <HomeListCard key={product.id} product={product} {...props} />
+            ))}
+        </div>
+
+        </>
+    )
+}
+export default HomePage
