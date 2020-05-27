d6a2b073f5ee63f32de7e86d91683f786da86b0b
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-05-27 13:32:59 -0500

    Added lin kto close order details

--- src/pages/customers/ProfilePages/OrderHistory.js
+++ src/pages/customers/ProfilePages/OrderHistory.js
@@ -3,17 +3,15 @@ import { Paper, PaymentForm } from "../../../components";
 import { PaymentDataManager } from "../../../modules";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-import { Sticky, Ref } from "semantic-ui-react";
+import { Sticky, Ref, Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import "../../../styles/OrderHistory.css";
+
 const OrderHistory = ({ itemId }) => {
   const classes = useStyles();
   const contextRef = createRef();
   // Order History needs to be stored in state as an array
-  const [orderHistory, setOrderHistory] = useState(
-    Array(30).fill(
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pretium purus quis odio venenatis, eget gravida dolor consequat. Donec ut leo semper, porttitor neque non, ornare dolor. Sed volutpat, libero sit amet cursus tristique, tortor risus commodo sem, a placerat magna nibh ut risus. Cras interdum dolor a leo pellentesque facilisis. In hac habitasse platea dictumst. Morbi sodales, leo at ornare rutrum, sapien leo hendrerit dui, sit amet rutrum nisl magna at nisl. Nunc posuere at nunc ac dapibus. Nam non augue eu dolor feugiat rutrum. Donec justo augue, posuere pellentesque nunc ultrices, mattis tincidunt tellus. Morbi interdum tellus sapien, at facilisis augue lobortis tristique. Nam lacinia lorem nisl, non suscipit enim lacinia vitae. Etiam eu sollicitudin augue, tristique blandit purus. Praesent ornare dui vitae"
-    )
-  );
+  const [orderHistory, setOrderHistory] = useState(Array(30).fill({ id: 1 }));
   // Set Loading in state
   const [loading, setLoading] = useState(false);
   //   State to hold what order the user is looking at
@@ -44,7 +42,8 @@ const OrderHistory = ({ itemId }) => {
               <Paper>
                 <div>Order Placed:</div>
                 <div>Price:</div>
-                <div>Details:</div>
+                <div>Pictures of order items</div>
+                <Link to={`/profile/order-history/${item.id}`}>Details</Link>
               </Paper>
             );
           })}
@@ -52,12 +51,17 @@ const OrderHistory = ({ itemId }) => {
         {itemId && (
           <Grid item xs={4}>
             <Paper classProps="order-details-column">
-              <div>Order Placed:</div>
+              <div className="paper-header">
+                <div>Order Placed</div>
+                <div>
+                  <Link to="/profile/order-history">
+                    <Icon name="x" />
+                  </Link>
+                </div>
+              </div>
               <div>Price:</div>
-              <div>Details:</div>
               <div>Products:</div>
               zxczxcz
-
             </Paper>
           </Grid>
         )}
