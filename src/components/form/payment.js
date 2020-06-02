d85e849a6da48a5cd835e03c966cfa4f7e616a10
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-06-02 14:46:35 -0500

    finished error messages

--- src/components/form/payment.js
+++ src/components/form/payment.js
@@ -1,81 +1,138 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import { Button } from "../buttons";
 import { Message } from "semantic-ui-react";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from "@material-ui/core/FormHelperText";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: 0,
+    padding: 0,
+  },
+  selectEmpty: {
+    // marginTop: theme.spacing(2),
+  },
+}));
 
 export default function PaymentForm({
   handleSubmit,
   handleChange,
   formState = { merchantName: "" },
   submitMessage,
+  monthChoices = [],
+  yearChoices = [],
+  errorMessage,
+  formErrors,
 }) {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Payment method
       </Typography>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} noValidate>
         <Grid container spacing={3}>
           <Grid item xs={12} md={4}>
-            <TextField
-              required
-              id="merchantName"
-              label="Merchant Name"
-              name="merchantName"
+            <FormControl
               fullWidth
-              value={formState.merchantName}
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <TextField
-              required
-              type="number"
-              id="cardNumber"
-              label="Card number"
-              name="accountNumber"
-              value={formState.accountNumber}
-              fullWidth
-              onChange={handleChange}
-            />
-          </Grid>
-          <Grid item xs={12} md={2}>
-            <InputLabel id="demo-simple-select-label">Month</InputLabel>
-            <Select
-              fullWidth
-              required
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={formState.expirationMonth}
-              onChange={handleChange}
+              error={formErrors.merchantName ? true : false}
             >
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
-              <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
+              <TextField
+                required
+                id="merchantName"
+                label="Merchant Name"
+                name="merchantName"
+                // fullWidth
+                value={formState.merchantName}
+                onChange={handleChange}
+              />
+              <FormHelperText>{formErrors.merchantName}</FormHelperText>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl
+              error={formErrors.accountNumber ? true : false}
+              fullWidth
+            >
+              <TextField
+                required
+                type="number"
+                id="cardNumber"
+                label="Card number"
+                name="accountNumber"
+                value={formState.accountNumber}
+                fullWidth
+                onChange={handleChange}
+              />
+              <FormHelperText>{formErrors.accountNumber}</FormHelperText>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} md={1}>
+            <FormControl
+              error={formErrors.expirationMonth ? true : false}
+              required
+              fullWidth
+            >
+              <InputLabel id="demo-simple-select-label">Month</InputLabel>
+              <Select
+                required
+                fullWidth
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={formState.expirationMonth}
+                onChange={handleChange}
+                name="expirationMonth"
+                inputProps={{
+                  required: true,
+                }}
+              >
+                <MenuItem value="">Select</MenuItem>
+                {monthChoices.map((month, i) => (
+                  <MenuItem key={i} value={month}>
+                    {month}
+                  </MenuItem>
+                ))}
+              </Select>
+              <FormHelperText>{formErrors.expirationMonth}</FormHelperText>
+            </FormControl>
           </Grid>
           <Grid item xs={12} md={2}>
-            <FormControl className={classes.formControl} disabled>
+            <FormControl
+              error={formErrors.expirationYear ? true : false}
+              required
+              fullWidth
+              disabled={formState.expirationMonth == "" ? true : false}
+            >
               <InputLabel id="demo-simple-select-label">Year</InputLabel>
               <Select
-                disabled={formState.expirationMonth !== "" ? true : false}
+                inputProps={{
+                  required: true,
+                }}
+                name="expirationYear"
                 fullWidth
-                required
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
                 value={formState.expirationYear}
                 onChange={handleChange}
               >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                <MenuItem value="">Select</MenuItem>
+                {yearChoices.map((year, i) => (
+                  <MenuItem key={i} value={year}>
+                    {year}
+                  </MenuItem>
+                ))}
               </Select>
+              <FormHelperText>{formErrors.expirationYear}</FormHelperText>
             </FormControl>
           </Grid>
           <Grid item xs={12} md={12}>
@@ -85,6 +142,13 @@ export default function PaymentForm({
             {submitMessage && (
               <Typography variant="h8" gutterBottom>
                 <Message positive>{submitMessage}</Message>
+              </Typography>
+            )}
+          </Grid>
+          <Grid item xs={12}>
+            {errorMessage && (
+              <Typography variant="h8" gutterBottom>
+                <Message negative>{errorMessage}</Message>
               </Typography>
             )}
           </Grid>
--- src/pages/customers/ProfilePages/AddPayment.js
+++ src/pages/customers/ProfilePages/AddPayment.js
@@ -7,32 +7,86 @@ const AddPaymentPage = () => {
     merchantName: "",
     accountNumber: "",
     expirationMonth: "",
-    expirationYear: ""
+    expirationYear: "",
   });
+  const [formErrors, setFormErrors] = useState({
+    merchantName: "",
+    accountNumber: "",
+    expirationMonth: "",
+    expirationYear: "",
+  });
+
+  const months = [...Array(12).keys()].map((x) => `${x + 1}`);
   const [submitMessage, setSubmitMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [monthChoices, setMonthChoices] = useState(months);
+  const [yearChoices, setYearChoices] = useState([]);
 
   const handleChange = (e) => {
+    if (submitMessage) {
+      setSubmitMessage("");
+    }
     const name = e.target.name;
     const value = e.target.value;
-
     setPaymentForm((prevState) => {
       let newObj = { ...prevState };
       newObj[name] = value;
       return newObj;
     });
+
+    if (name == "expirationMonth") {
+      let today = new Date();
+      let month = today.getMonth();
+      let year = today.getFullYear();
+      if (value > month) {
+        setYearChoices([...Array(21).keys()].map((x) => x + Number(year)));
+      } else {
+        setYearChoices([...Array(20).keys()].map((x) => x + Number(year) + 1));
+      }
+    }
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     const token = window.sessionStorage.getItem("token");
-    PaymentDataManager.addPayment(token, {
-      merchant_name: paymentForm.merchantName,
-      account_number: paymentForm.accountNumber,
-      expiration_date: paymentForm.expirationDate,
-    }).then((resp) => {
-      if (resp.status === 200) {
-        setSubmitMessage("Payment Added");
+    let errors = [];
+    Object.keys(paymentForm).map((item) => {
+      if (paymentForm[item] == "") {
+        errors.push([item, "Required"]);
       }
     });
+
+    if (errors.length > 0) {
+      setFormErrors((prevState) => {
+        let newObj = {
+          merchantName: "",
+          accountNumber: "",
+          expirationMonth: "",
+          expirationYear: "",
+        };
+        errors.map((item) => {
+          newObj[item[0]] = item[1];
+        });
+        return newObj;
+      });
+    } else {
+      PaymentDataManager.addPayment(token, {
+        merchant_name: paymentForm.merchantName,
+        account_number: paymentForm.accountNumber,
+        expiration_date: `${paymentForm.expirationYear}-${paymentForm.expirationMonth}-01`,
+      }).then((resp) => {
+        if (resp.status === 200) {
+          setSubmitMessage("Payment Added");
+          let clearObj = {
+            merchantName: "",
+            accountNumber: "",
+            expirationMonth: "",
+            expirationYear: "",
+          };
+          setPaymentForm(clearObj);
+          setFormErrors(clearObj);
+        }
+      });
+    }
   };
 
   return (
@@ -42,6 +96,10 @@ const AddPaymentPage = () => {
         handleChange={handleChange}
         handleSubmit={handleSubmit}
         submitMessage={submitMessage}
+        monthChoices={monthChoices}
+        yearChoices={yearChoices}
+        errorMessage={errorMessage}
+        formErrors={formErrors}
       />
     </Paper>
   );
