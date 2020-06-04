befb2a92e37e6907e6f4a557e510369fc5b2a764
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-06-04 16:07:22 -0500

    styled pages grid with verticla menus

--- src/pages/reports/Reports.js
+++ src/pages/reports/Reports.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { VerticalMenu } from "../../components";
-import {MultipleOpenReports} from "./index";
+import { MultipleOpenReports } from "./index";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 
@@ -19,7 +19,7 @@ const Reports = ({ match }) => {
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={2} md={2}>
+        <Grid item xs={12} md={3}>
           <VerticalMenu
             menuData={[
               { title: "Multiple Orders", route: "/reports/multiple-orders" },
@@ -27,10 +27,11 @@ const Reports = ({ match }) => {
             firstActive={reportsView}
           />
         </Grid>
-        <Grid item xs={8} md={9}>
+        <Grid item xs={12} md={8}>
           {reportsView === "" && "Choose a report to run"}
-          {reportsView === "multiple-orders" && <MultipleOpenReports/>}
+          {reportsView === "multiple-orders" && <MultipleOpenReports />}
         </Grid>
+        <Grid item xs={0} md={1}></Grid>
       </Grid>
     </div>
   );
