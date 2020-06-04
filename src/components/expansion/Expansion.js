a47a5926f3bc0d2a4950ee1704bb00f0d8add3f9
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-06-04 15:17:56 -0500

    finished styling

--- src/components/expansion/Expansion.js
+++ src/components/expansion/Expansion.js
@@ -5,7 +5,7 @@ import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-
+import "./expansion.css";
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
@@ -30,10 +30,10 @@ export default function SimpleExpansionPanel({
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography className={classes.heading}>{summary}</Typography>
+          <Typography className="expansion-panel-summary">{summary}</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <Typography>{details}</Typography>
+          <Typography className="expansion-panel-summary">{details}</Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
     </div>
